9599abc272f230280daa68f3e7fdc18a136cc1ad
Yuriy <yuriy_vm@inbox.ru>
2024-02-27 22:21:10 +0300

    Completed Task_3

--- Task_2/obj/Debug/net8.0/Task_2.AssemblyInfo.cs
+++ Task_2/obj/Debug/net8.0/Task_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9798147cac84b1b21214bf938b450a07606fbbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39f8f63df95d84090f7372f442203e3ead239d08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача:
+Задайте произвольную строку.
+Выясните, является ли она палиндромом.
+*/
+void Main()
+{
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+Console.InputEncoding = System.Text.Encoding.GetEncoding(1251);
+
+Console.Write("Введите слово: ");
+string? input = Console.ReadLine();
+string temp = input!;
+
+int polindromFalse = 0;
+int a = input!.Length - 1;
+
+for (int i = 0; i < input.Length / 2; i++)
+{
+    if (input[i] == temp[a])
+        a--;
+    else
+    {
+        polindromFalse += 1;
+        a--;
+    }
+}
+if (polindromFalse > 0)
+    Console.WriteLine("Слово не является палиндромом");
+else
+    Console.WriteLine("Это палиндром");
+}
+Main();
